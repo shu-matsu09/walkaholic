6114743fd415e13975dbfbc21f9320b6ea275900
shu-matsu09 <shusukem1988@gmail.com>
2021-05-19 13:12:32 +0900

    Revert "Revert "ルーティングの設定""
    This reverts commit 849f992a54898957385dd942e7365ea12f13c1bb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pedometers/index'
+  get 'pedometers/new'
+  root to: 'pedometers#index'
+  resources :pedometers, only: [:index, :new, :create]
 end
