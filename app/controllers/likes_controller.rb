f3d7f3a3d27087cd227771e61b9afc78aadb6bff
shu-matsu09 <shusukem1988@gmail.com>
2021-06-10 19:04:49 +0900

    非同期いいねの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,17 @@
 class LikesController < ApplicationController
+  before_action :pedometer_params
+  
   def create
     Like.create(user_id: current_user.id, pedometer_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, pedometer_id: params[:id]).destroy
-    redirect_to root_path
+  end
+
+  private
+
+  def pedometer_params
+    @pedometer = Pedometer.find(params[:id])
   end
 end
