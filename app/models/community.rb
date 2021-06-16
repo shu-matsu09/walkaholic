d006a04fa3e547d788923918d8d19635b4f069d2
shu-matsu09 <shusukem1988@gmail.com>
2021-06-16 17:41:30 +0900

    コミュニティ編集・削除・参加機能の実装

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,5 +2,7 @@ class Community < ApplicationRecord
   has_many :community_users, dependent: :destroy
   has_many :users, through: :community_users
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true,
+                   length: { maximum: 20 }
+  validates :detail, presence: true, length: { maximum: 200 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pedometers#index'
   resources :pedometers, only: [:index, :new, :create]
-  resources :communities, only: [:index, :new, :create]
+  resources :communities do
+    member do
+      post 'add_user'
+    end
+  end
   resources :users, only: :show
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
