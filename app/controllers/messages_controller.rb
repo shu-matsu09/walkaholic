41e7325ae7d0258f98b100965154c9a4bae5536f
shu-matsu09 <shusukem1988@gmail.com>
2021-06-23 18:16:43 +0900

    メッセージUIの変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @community = Community.find(params[:community_id])
     @message = Message.new
-    @messages = @community.messages.includes(:user)
+    @messages = @community.messages.includes(:user).order("created_at DESC")
   end
 
   def create
